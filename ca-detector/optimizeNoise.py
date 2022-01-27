3ad168a25992401a16e7734d79f75c3751727d5e
eunicen1 <ee.nwaobi@gmail.com>
2022-01-26 22:52:42 -0800

    Denoising tests possibly complete.

--- ca-detector/optimizeNoise.py
+++ ca-detector/optimizeNoise.py
@@ -1,30 +1,43 @@
-from psnr import signaltonoise as snr
+from snr import signaltonoise as sn
 from denoise import denoise
 from waveletDenoise import waveletDenoise as wdenoise
+# from waveletDenoise import waveletDenoiseTest
 from statistics import mean
 import os
 import numpy as np
+import pywt
 
 os.chdir('ecgiddb')
 orig = []
 snrs = []
 snrs_wv = []
+sns = []
 for norm in os.listdir():
     tfile = open(norm, "r")
     lines = (tfile.read().splitlines())
     arr = np.float64(lines)
     xnorm = denoise(arr)
     wnorm = wdenoise(arr)
-    orig.append(snr(arr))
-    snrs.append(snr(xnorm))
-    snrs_wv.append(snr(wnorm))
 
-print(orig, snrs, snrs_wv)
+    orig.append(sn(arr))
+    snrs.append(sn(xnorm))
+    snrs_wv.append(sn(wnorm))
+
+#     trials = waveletDenoiseTest(arr)
+#     s = mean(orig)
+#     for t in trials:
+#         m = np.mean(sn(t))
+#         snval = abs(m*100/(s-m))
+#         sns.append(snval)
+#
+# highest = list(pywt.wavelist(kind='discrete'))[np.argmax(sns)]
+# print(np.argmax(sns), highest, sns[np.argmax(sns)])
+
 s = mean(orig)
 m1 = mean(snrs)
 m2 = mean(snrs_wv)
-sn1 = m1*100/(s-m1) # should I abs()?
-sn2 = m2*100/(s-m2) # should I abs()?
+sn1 = abs(m1*100/(s-m1))
+sn2 = abs(m2*100/(s-m2))
 print("Mean signaltonoise for noisy array: " + str(s))
 print("Mean signaltonoise for ecgiddb and original: " + str(m1) + " and SNR of: " + str(sn1))
 print("Mean signaltonoise for ecgiddb and wavelets: " + str(m2) + " and SNR of: " + str(sn2))
--- ca-detector/waveletDenoise.py
+++ ca-detector/waveletDenoise.py
@@ -7,19 +7,38 @@ from denoise import denoise
 
 def waveletDenoise(arr):
     #print(pywt.wavelist(kind='discrete'))
-    return (dwt(arr, 'bior1.1')[0])
+    return (dwt(arr, 'bior1.5')[0])
 
+def waveletDenoiseTest(arr):
+    wvlist = []
+    for type in list(pywt.wavelist(kind='discrete')):
+        wvlist.append(dwt(arr, type)[0])
+    return wvlist
 # ['bior1.1', 'bior1.3', 'bior1.5', 'bior2.2', 'bior2.4', 'bior2.6', 'bior2.8', 'bior3.1', 'bior3.3', 'bior3.5', 'bior3.7', 'bior3.9', 'bior4.4', 'bior5.5', 'bior6.8', 'coif1', 'coif2', 'coif3', 'coif4', 'coif5', 'coif6', 'coif7', 'coif8', 'coif9', 'coif10', 'coif11', 'coif12', 'coif13', 'coif14', 'coif15', 'coif16', 'coif17', 'db1', 'db2', 'db3', 'db4', 'db5', 'db6', 'db7', 'db8', 'db9', 'db10', 'db11', 'db12', 'db13', 'db14', 'db15', 'db16', 'db17', 'db18', 'db19', 'db20', 'db21', 'db22', 'db23', 'db24', 'db25', 'db26', 'db27', 'db28', 'db29', 'db30', 'db31', 'db32', 'db33', 'db34', 'db35', 'db36', 'db37', 'db38', 'dmey', 'haar', 'rbio1.1', 'rbio1.3', 'rbio1.5', 'rbio2.2', 'rbio2.4', 'rbio2.6', 'rbio2.8', 'rbio3.1', 'rbio3.3', 'rbio3.5', 'rbio3.7', 'rbio3.9', 'rbio4.4', 'rbio5.5', 'rbio6.8', 'sym2', 'sym3', 'sym4', 'sym5', 'sym6', 'sym7', 'sym8', 'sym9', 'sym10', 'sym11', 'sym12', 'sym13', 'sym14', 'sym15', 'sym16', 'sym17', 'sym18', 'sym19', 'sym20']
-#
+
 # chdir("ecgiddb")
-# tfile = open("Person_06.txt")
+# tfile = open("Person_01.txt")
 # lines = (tfile.read().splitlines())
 # array = np.float64(lines)
-# xnorm = waveletDenoise(array)
-# print(xnorm)
+# den1 = denoise(array)
+# den2 = waveletDenoise(array)
 #
-# plt.plot(denoise(array))
+# plt.plot(np.linspace(0,20,num=len(den1)), den1)
+# plt.xlabel("Time in seconds")
+# plt.ylabel("Voltage in mV")
+# plt.title("Denoised Signal Before Optimization for Person_01.txt")
 # plt.show()
-# plt.plot(xnorm)
+#
+#
+# plt.plot(np.linspace(0,20,num=len(den2)), den2)
+# plt.xlabel("Time in seconds")
+# plt.ylabel("Voltage in mV")
+# plt.title("Denoised Signal After Optimization for Person_01.txt")
 # plt.show()
-# #plt.show()
+# plt.show()
+
+# plt.plot(np.linspace(0,20,num=len(array)), array)
+# plt.xlabel("Time in seconds")
+# plt.ylabel("Voltage in mV")
+# plt.title("Noisy Signal from Person_01.txt")
+# plt.show()
